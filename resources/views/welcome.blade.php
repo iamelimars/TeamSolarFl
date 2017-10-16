9902b45a7d817c1859b1f432751add2117522fd0
Eli MArshall <elimarshall@Elis-Mac-mini.local>
2017-10-16 18:29:59 -0400

    Added frontend for Team section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,27 +46,17 @@
     <div class="container">
       <h3>Contact</h3>
       <form id="contact-form" class="col-xs-10 col-xs-offset-1" action="{!! route('post.contact') !!}" method="post">
-        <label class="col-xs-12" for="name">Name:</label>
+        <label class="col-xs-12" for="name">Name</label>
         <input class="col-xs-12" type="text" id="name" name="name" value="">
 
-        <label class="col-xs-12" for="email">Email:</label>
+        <label class="col-xs-12" for="email">Email</label>
         <input class="col-xs-12" type="email" name="email" id="email" value="">
 
-        <label class="col-xs-12" for="body">Body:</label>
+        <label class="col-xs-12" for="body">Body</label>
         <textarea class="col-xs-12" name="body" id="body" rows="8" cols="80"></textarea>
 
         <input type="submit" value="Submit" id="submit" class="col-xs-4 col-xs-offset-4 ">
       </form>
-        {{--  {!! Form::open(['url' => '', 'method' => 'post']) !!}  --}}
-        {{--  {!! Form::open(array('route' => 'post.contact', 'id' => 'contactForm')) !!}
-            {{ csrf_field() }}
-            <div class="col-xs-12">
-                {{ Form::text('contactName', null, array('class' => 'col-xs-12', 'placeholder' => 'Name')) }}
-                {{ Form::email('contactEmail', null, array('class' => 'col-xs-12', 'placeholder' => 'Email')) }}
-                {{ Form::textarea('contactMessage', null, array('class' => 'col-xs-12', 'placeholder' => 'Enter Message Here')) }}
-                {{ Form::submit('Submit', ['class' => 'col-xs-4 col-xs-offset-4 submit']) }}
-            </div>
-        {!! Form::close() !!}  --}}
     </div>
 </div>
 
@@ -74,8 +64,36 @@
 
 <div id="team" class="section">
     <div class="container">
-
+      <h3>Team</h3>
+      <div class="col-xs-12 col-sm-4 team-container">
+        <div class="col-xs-12 team-card">
+          <img class="col-xs-6 col-xs-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3" src="https://s3.us-east-2.amazonaws.com/iamelimars/Walter%403x.png" alt="">
+          <h4 class="col-xs-12 text-center">Walter Briceno</h4>
+          <h5 class=" col-xs-12 text-center">CEO</h5>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 team-container">
+        <div class="col-xs-12 team-card">
+          <img class="col-xs-6 col-xs-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3" src="https://s3.us-east-2.amazonaws.com/iamelimars/Sonia%403x.png" alt="">
+          <h4 class="col-xs-12 text-center">Sonia Cera</h4>
+          <h5 class=" col-xs-12 text-center">COO</h5>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 team-container">
+        <div class="col-xs-12 team-card">
+          <img class="col-xs-6 col-xs-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3" src="https://s3.us-east-2.amazonaws.com/iamelimars/diana%403x.png" alt="">
+          <h4 class="col-xs-12 text-center">Diana Briceno</h4>
+          <h5 class=" col-xs-12 text-center">CMO</h5>
+        </div>
+      </div>
     </div>
+</div>
+
+<div id="footer" class="top-footer">
+
+</div>
+<div id="bottom-footer" class="footer">
+
 </div>
 
 @endsection
